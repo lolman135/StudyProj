6979f2d6d7ecccd27aa43dcc369fa1557d6455c3
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2024-08-23 19:51:20 +0300

    Add car class

--- src/main/java/ua/lesson4/Main.java
+++ src/main/java/ua/lesson4/Main.java
@@ -1,4 +1,10 @@
 package ua.lesson4;
 
+import ua.lesson4.car.Car;
+
 public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Zaporozhec", 1982, "AE 1023 XP");
+        System.out.println(car.carCode);
+    }
 }
--- src/main/java/ua/lesson4/car/Car.java
+++ src/main/java/ua/lesson4/car/Car.java
@@ -1,5 +1,17 @@
 package ua.lesson4.car;
 
 public class Car {
+    private String modelName;
+    int year;
+    protected boolean engineStarted;
+    public String carCode;
+    private double fuelLevel;
 
+    public Car(String modelName, int year, String carCode) {
+        this.modelName = modelName;
+        this.year = year;
+        this.carCode = carCode;
+        engineStarted = false;
+        fuelLevel = 50.0;
+    }
 }
